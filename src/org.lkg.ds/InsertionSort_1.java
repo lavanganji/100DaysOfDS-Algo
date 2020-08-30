5d50471009ffa67eb325b7fec0eee4e60cabaf6f
Lavan Kumar <11154769+lavanganji@users.noreply.github.com>
2020-08-30 02:15:44 -0500

    Update InsertionSort_1.java

--- src/org.lkg.ds/InsertionSort_1.java
+++ src/org.lkg.ds/InsertionSort_1.java
@@ -9,6 +9,21 @@ public class InsertionSort_1 {
 		System.out.println("Before SORT : "+Arrays.toString(arr));
 		insertionSort(arr);
 		System.out.println("After  SORT : "+Arrays.toString(arr));
+		insertionSortDesc(arr);
+		System.out.println("After  SORT : "+Arrays.toString(arr));
+	}
+
+	private static void insertionSortDesc(int[] arr) {
+		// TODO Auto-generated method stub
+		for (int j=1 ; j<arr.length;j++) {
+			int key = arr[j];
+			int i = j-1;
+			while(i>=0 && arr[i]<key) {
+				arr[i+1] = arr[i];
+				i--;
+			}
+			arr[i+1] = key;
+		}
 	}
 
 	private static void insertionSort(int[] arr) {
@@ -23,4 +38,5 @@ public class InsertionSort_1 {
 			arr[i+1] = key;
 		}
 	}
+
 }
